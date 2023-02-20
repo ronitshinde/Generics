d880f379877a51650022a59d10a2d5b973dfacbd
ronitshinde <rony.shinde753@gmail.com>
2023-02-20 08:05:24 +0530

    [Ronit] Add : UC1-Maximum Of Integers

---
+++ Generics/MaxOfNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MaxOfNumbers
+    {
+        public int MaxInteger(int numOne, int numTwo, int numThree)
+        {
+            if (numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) > 0)
+            {
+                return numOne;
+            }
+            if (numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) > 0)
+            {
+                return numTwo;
+            }
+            if (numThree.CompareTo(numOne) > 0 && numThree.CompareTo(numTwo) > 0)
+            {
+                return numThree;
+            }
+            throw new Exception("numOne,numTwo,numThree are same");
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding maximum of three numbers");
+            MaxOfNumbers integer = new MaxOfNumbers();
+            int maximumNum = integer.MaxInteger(31, 21, 11);
+            Console.WriteLine("Maximum Number is :" + maximumNum);
+            Console.ReadLine();
+        }
+    }
+}
