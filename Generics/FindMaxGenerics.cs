334ff5da23e7e6e4310647ae169e25c5e4774dc5
ronitshinde <rony.shinde753@gmail.com>
2023-02-22 06:41:41 +0530

    [Ronit] Add : Test file added

---
+++ Generics/FindMaxGenerics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    // icomparible is used to provide a default sort order for your obj(object).
+   
+    public class FindMaxGeneric<T> where T : IComparable //method is generic
+    {
+        public T FindMaximumGeneric(T a, T b, T c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+            {
+                return a;
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                return b;
+            }
+            else
+                return c;
+        }
+    }
+}
+    
+
+
